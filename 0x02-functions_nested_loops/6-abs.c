8d5afeacd687af31b92c0517d72ccc8969910b64
Theophilus Akpan <akpantheophilus00@gmail.com>
2022-12-13 20:00:20 -0800

    task-6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * _abs - computes the absolute value of an interger
+ * @n: THe int to be operated upon
+ *
+ * Return: returns an unsigned int value to calling function
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+
+	else 
+	{
+	n *= -1;
+	return (n);
+	}
+}
+
+
